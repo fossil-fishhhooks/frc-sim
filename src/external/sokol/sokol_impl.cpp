8f4c54089f7f8b95f159e81f4047499aa3cfa159
Arin <arin.jander@student.csd509j.net>
2026-06-19 21:25:59 -0700

    sokol

---
+++ src/external/sokol/sokol_impl.cpp
@@ -0,0 +1,15 @@
+#define SOKOL_IMPLEMENTATION
+#define SOKOL_GFX_IMPL
+#define SOKOL_APP_IMPL
+#define SOKOL_GLUE_IMPL
+#define SOKOL_TIME_IMPL
+#define SOKOL_LOG_IMPL
+#define SOKOL_DEBUGTEXT_IMPL
+
+#define SOKOL_GLCORE
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_glue.h"
+#include "sokol_time.h"
+#include "sokol_log.h"
+#include "sokol_debugtext.h"
